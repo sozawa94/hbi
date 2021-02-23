436a443fff4d89b7ce218701bc05ecb1e9c6ff08
So Ozawa <so.danmaku.oz@gmail.com>
2021-02-23 20:52:43 +0900

    changed constants

--- m_const.f90
+++ m_const.f90
@@ -3,8 +3,8 @@ module mod_constant
  real(8),parameter::pi=4*datan(1.d0)
  real(8),parameter::pois=0.25d0
  real(8),parameter::sigma0=100d0
- real(8),parameter::vref=1.d-9
- real(8),parameter::rigid=40d0
+ real(8),parameter::vref=1.d-6
+ real(8),parameter::rigid=32.04d0
  real(8),parameter::vs=3.464d0
  real(8),parameter::vp=6.d0
  !real(8),parameter::tr=1d12
