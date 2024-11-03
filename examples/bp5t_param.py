b30f2cc262a86d5de4d9c022f818a249252a8847
So Ozawa <57583648+sozawa94@users.noreply.github.com>
2024-11-03 14:17:08 +0900

    Update bp5t_param.py

--- examples/bp5t_param.py
+++ examples/bp5t_param.py
@@ -1,119 +1,56 @@
-import math
+#parameter distribution (for triangular mesh)
+#!cd /work/hp220105o/i25004/hbi
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Constants and parameters
-n = 9250
-pi = math.pi
-ds0 = 1.0
-mu0 = 0.6
-vref = 1e-6
-vs = 3.464
-velinit = 1e-9
-rigid = 32.04
-
-# Arrays
-xcol = np.zeros(n)
-ycol = np.zeros(n)
-zcol = np.zeros(n)
-ang = np.zeros(n)
-angd = np.zeros(n)
-rake = np.zeros(n)
-xr = np.zeros(n)
-yr = np.zeros(n)
-zr = np.zeros(n)
-ds = np.zeros(n)
-a = np.zeros(n)
-b = np.zeros(n)
-dc = np.zeros(n)
-f0 = np.zeros(n)
-vel = np.zeros(n)
-sigma = np.zeros(n)
-tau = np.zeros(n)
-mu = np.zeros(n)
-taudot = np.zeros(n)
-sigmadot = np.zeros(n)
-xs1 = np.zeros(n)
-xs2 = np.zeros(n)
-xs3 = np.zeros(n)
-ys1 = np.zeros(n)
-ys2 = np.zeros(n)
-ys3 = np.zeros(n)
-zs1 = np.zeros(n)
-zs2 = np.zeros(n)
-zs3 = np.zeros(n)
-
-# Geometry
-with open('bp5t.stl', 'r') as file:
-    lines = file.readlines()[1:]
-
-k = 0
-for i in range(0, len(lines), 7):
-    if i + 6 >= len(lines):
-        break
-    k += 1
-    if k >= n:
-        break
-    # print(lines[i + 3].split())
-    # print(lines[i + 3].split()[-3])
-    xs1[k] = float(lines[i + 2].split()[-3])
-    xs1[k] = float(lines[i + 2].split()[-3])
-    ys1[k] = float(lines[i + 2].split()[-2])
-    zs1[k] = float(lines[i + 2].split()[-1])
-    xs2[k] = float(lines[i + 3].split()[-3])
-    ys2[k] = float(lines[i + 3].split()[-2])
-    zs2[k] = float(lines[i + 3].split()[-1])
-    xs3[k] = float(lines[i + 4].split()[-3])
-    ys3[k] = float(lines[i + 4].split()[-2])
-    zs3[k] = float(lines[i + 4].split()[-1])
-    
-    xcol[k] = (xs1[k] + xs2[k] + xs3[k]) / 3
-    ycol[k] = (ys1[k] + ys2[k] + ys3[k]) / 3
-    zcol[k] = (zs1[k] + zs2[k] + zs3[k]) / 3
-    
-    #print(xcol[k], ycol[k], zcol[k])
-
-print(k-1)
+ncell=9250
+x=np.zeros(ncell);z=np.zeros(ncell)
+with open('/work/hp220105o/i25004/hbi/examples/bp5t.stl') as f: #need to be changed
+    lines = f.read()
+    d=lines.split()
+    print(d[12])
+    for k in range(ncell):
+        xs1=float(d[4+21*k+8]);xs2=float(d[4+21*k+12]);xs3=float(d[4+21*k+16])
+        zs1=float(d[4+21*k+10]);zs2=float(d[4+21*k+14]);zs3=float(d[4+21*k+18])
+        x[k]=(xs1+xs2+xs3)/3
+        z[k]=(zs1+zs2+zs3)/3
 
 # Frictional parameters
 a0 = 0.004
 b0 = 0.03
 dc0 = 0.14
 a_max = 0.04
+mu0=0.6
+vref=1e-6
+sigma0=25.0
+rigid=32.04
+cs=3.464
+vel0=1e-9
+tau0=sigma0*a_max*np.arcsinh(0.5*vel0/vref*np.exp((mu0+b0*np.log(vref/vel0))/a_max))#+rigid/(2*Vs)*vel(i)
+eta=rigid/2/cs
 
-with open('bp5t_param.dat', 'w') as file:
-    for i in range(n):
-        f0[i] = mu0
-        dep = -zcol[i]
+a=np.zeros(ncell);dc=np.zeros(ncell)
+tau=np.zeros(ncell);vel=np.zeros(ncell)
 
-        if 4 < dep < 16 and abs(xcol[i]) < 30:
-            a[i] = a0
-        elif dep < 2 or dep > 18 or abs(xcol[i]) > 32:
-            a[i] = a_max
-        else:
-            r = max(abs(dep - 10) - 6, abs(xcol[i]) - 30) / 2
-            a[i] = a0 + r * (a_max - a0)
-        
-        r = max(abs(dep - 10) - 6, abs(xcol[i]) - 30) / 2
-        a[i] = min(a0 + r * (a_max - a0), a_max)
-        a[i] = max(a[i], a0)
+for i in range(ncell):
+    dep = -z[i]
+    dc[i]=dc0
+    r = max(abs(dep - 10) - 6, abs(x[i]) - 30) / 2
+    a[i] = min(a0 + r * (a_max - a0), a_max)
+    a[i] = max(a[i], a0)
+    vel[i]=vel0
+    if abs(x[i] + 24) < 6 and abs(dep - 10) < 6:
+        vel[i] = 3e-2
+    if abs(x[i] + 24) < 6 and abs(dep - 10) < 6:
+        dc[i] = 0.13
+    
+    tau[i] = sigma0*(a[i]*np.arcsinh(0.5*vel[i]/vref*np.exp((mu0+b0*np.log(vref/vel0))/a[i]))+rigid/(2*cs)*vel[i])
+    #f.write(0.0),str(a[i]))#,b0,dc0,f0,tau0,sigma0,vel0,0.0,0.0)
+plt.scatter(x,z,c=vel,s=10)
+plt.colorbar()
 
-        b[i] = b0
-        dc[i] = dc0
-
-        if abs(xcol[i] + 24) < 6 and abs(dep - 10) < 6:
-            dc[i] = 0.13
-
-        sigma[i] = 25.0
-        vel[i] = velinit
-        dep = -zcol[i]
-        if abs(xcol[i] + 24) < 6 and abs(dep - 10) < 6:
-            vel[i] = 3e-2
-        
-        mu[i] = a[i] * math.asinh(0.5 * vel[i] / vref * math.exp((f0[i] + b[i] * math.log(vref / velinit)) / a[i])) + rigid / (2 * vs) * vel[i]
-        tau[i] = mu[i] * sigma[i]
-
-        taudot[i] = 0.0
-        sigmadot[i] = 0.0
-
-        file.write(f'{0.0:17.8e} {a[i]:17.8e} {b[i]:17.8e} {dc[i]:17.8e} {f0[i]:17.8e} {tau[i]:17.8e} {sigma[i]:17.8e} {vel[i]:17.8e} {taudot[i]:17.8e} {sigmadot[i]:17.8e}\n')
-
+data=(a,dc,tau,vel)
+data=np.transpose(data)
+df=pd.DataFrame(data, columns=['a','dc','tau','vel'])
+df.to_csv('../examples/bp5t_param.dat', sep='\t', index=False)
