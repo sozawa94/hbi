3186699a60ba9a770cf3fe2bb8e7266b5fac959b
sozawa94 <so.danmaku.oz@gmail.com>
2024-11-03 15:00:57 +0900

    revised example input files

---
+++ examples/bp3dip30thrust_param.py
@@ -0,0 +1,29 @@
+#parameter distribution for BP3 dip 30 thrust
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ncell=1600
+ds=0.025
+H=15.0
+h=3.0
+a_max=0.025
+a0=0.010
+a=np.zeros(ncell)
+x=np.zeros(ncell)
+
+for k in range(ncell):
+    x[k]=(k+0.5)*ds
+    if x[k]<H:
+        a[k]=a0
+    else:
+        a[k]=min(a_max,a0+(a_max-a0)*(x[k]-H)/h)
+
+#plt.plot(x,a)
+#plt.xlim([15,20])
+
+data=(a)
+data=np.transpose(data)
+df=pd.DataFrame(data, columns=['a'])
+print(df)
+df.to_csv('../examples/bp3dip30thrust_param.dat', sep='\t', index=False)
